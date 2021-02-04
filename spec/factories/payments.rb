bfc8b649016e76eaf3c1b043426fc3876e0c25b5
Santiago Ramirez <santiago.ramirez@rootstrap.com>
2021-02-04 18:11:29 -0300

    create payment model

---
+++ spec/factories/payments.rb
@@ -0,0 +1,26 @@
+# == Schema Information
+#
+# Table name: payments
+#
+#  id          :integer          not null, primary key
+#  sender_id   :integer          not null
+#  receiver_id :integer          not null
+#  amount      :float            default("0.0"), not null
+#  description :text
+#  created_at  :datetime         not null
+#  updated_at  :datetime         not null
+#
+# Indexes
+#
+#  index_payments_on_receiver_id  (receiver_id)
+#  index_payments_on_sender_id    (sender_id)
+#
+
+FactoryBot.define do
+  factory :payment do
+    sender      factory: :user
+    receiver    factory: :user
+    amount      { Faker::Number.within(range: 1..999) }
+    description { Faker::Lorem.words(rand(0..20)) }
+  end
+end
